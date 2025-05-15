af8d04ff5a37e6ce7c520ca91759c60c98d1bb5a
jainchidrup <punit@editorschamber.in>
2025-05-15 13:18:10 +0530

    chatting folder insert

--- lib/views/home_view/widgets/menu_popup.dart
+++ lib/views/home_view/widgets/menu_popup.dart
@@ -16,8 +16,7 @@ class MenuPopup extends StatefulWidget {
   State<MenuPopup> createState() => _MenuPopupState();
 }
 
-class _MenuPopupState extends State<MenuPopup>
-    with SingleTickerProviderStateMixin {
+class _MenuPopupState extends State<MenuPopup> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<Offset> _slideAnimation;
   late Animation<double> _fadeAnimation;
@@ -89,24 +88,30 @@ class _MenuPopupState extends State<MenuPopup>
   Widget build(BuildContext context) {
     return BackdropFilter(
       filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
-      child: WillPopScope(
-        onWillPop: () async {
-          await _animationController.reverse();
-          return true;
+      child: GestureDetector(
+        onTap: () {
+          _animationController.reverse().then((_) => Get.back());
         },
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topLeft, // Align to top-left
-              child: SlideTransition(
-                position: _slideAnimation,
-                child: FadeTransition(
-                  opacity: _fadeAnimation,
-                  child: _buildMenu(context),
+        behavior: HitTestBehavior.opaque,
+        child: WillPopScope(
+          onWillPop: () async {
+            await _animationController.reverse();
+            return true;
+          },
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: SlideTransition(
+                  position: _slideAnimation,
+                  child: FadeTransition(
+                    opacity: _fadeAnimation,
+                    child: _buildMenu(context),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -115,108 +120,87 @@ class _MenuPopupState extends State<MenuPopup>
   Widget _buildMenu(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        width: 260.w,
-        decoration: BoxDecoration(
-          color: colorScheme.onPrimary,
-          borderRadius: BorderRadius.circular(10.r),
-          boxShadow: [
-            BoxShadow(
-              color: colorScheme.outlineVariant,
-              blurRadius: 10.r,
-              offset: const Offset(0, 4),
+    final menuItems = [
+      _MenuItem(
+        imagePath: 'assets/icons/account.png',
+        title: 'My Account',
+        onTap: () => _navigateAndClose(AppRoutes.account),
+      ),
+      _MenuItem(
+        imagePath: 'assets/icons/shift_req.png',
+        title: 'Shift Request',
+        onTap: () => _navigateAndClose(AppRoutes.shiftRequest),
+      ),
+      _MenuItem(
+        imagePath: 'assets/icons/forms.png',
+        title: 'Forms',
+        onTap: () => _navigateAndClose(AppRoutes.formView),
+      ),
+      _MenuItem(
+        imagePath: 'assets/icons/privacy.png',
+        title: 'Privacy Policy',
+        onTap: () => _navigateAndClose(AppRoutes.privacy),
+      ),
+      _MenuItem(
+        imagePath: 'assets/icons/chatting.png',
+        title: 'Chatting Home',
+        onTap: () => _navigateAndClose(AppRoutes.chattingHome),
+      ),
+    ];
+
+    return GestureDetector(
+      onTap: () {},
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          width: 280.w,
+          decoration: BoxDecoration(
+            color: colorScheme.onPrimary,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(16.r),
+              bottomRight: Radius.circular(16.r),
             ),
-          ],
-        ),
-        margin: EdgeInsets.only(left: 16.w, top: 35.h, bottom: 16.h), // Adjusted margin for left
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 8.h),
-              _buildPopupHeader(context),
-              _buildMenuItem(
-                context,
-                _MenuItem(
-                  imagePath: 'assets/icons/account.png',
-                  title: "My Account",
-                  onTap: () => _navigateAndClose(AppRoutes.account),
-                ),
+            boxShadow: [
+              BoxShadow(
+                color: colorScheme.outlineVariant.withOpacity(0.3),
+                blurRadius: 15.r,
+                spreadRadius: 2.r,
+                offset: const Offset(2, 0),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Obx(() => Text(
-                  homeController.userEmail.value.isNotEmpty
-                      ? homeController.userEmail.value
-                      : '$user',
-                  style: TextStyle(
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w500,
-                    color: colorScheme.onSurface,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                )),
-              ),
-              const Divider(),
-              _buildMenuItem(
-                context,
-                _MenuItem(
-                  imagePath: 'assets/icons/shift_req.png',
-                  title: "Shift Request",
-                  onTap: () => _navigateAndClose(AppRoutes.shiftRequest),
-                ),
-              ),
-              _buildMenuItem(
-                context,
-                _MenuItem(
-                  imagePath: 'assets/icons/forms.png',
-                  title: "Forms",
-                  onTap: () => _navigateAndClose(AppRoutes.formView),
-                ),
-              ),
-              _buildMenuItem(
-                context,
-                _MenuItem(
-                  imagePath: 'assets/icons/privacy.png',
-                  title: "Privacy Policy",
-                  onTap: () => _navigateAndClose(AppRoutes.privacy),
-                ),
-              ),
-              const Divider(),
-              ListTile(
-                leading: Container(
-                  width: 36.w,
-                  height: 36.h,
-                  padding: EdgeInsets.all(5.w),
-                  decoration: BoxDecoration(
-                    color: colorScheme.errorContainer,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Center(
-                    child: Icon(
-                      Icons.logout,
-                      size: 16.sp,
-                      color: colorScheme.error,
-                    ),
-                  ),
-                ),
-                title: Text(
-                  'Logout',
-                  style: TextStyle(
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.bold,
-                    color: colorScheme.onSurface,
-                  ),
-                ),
-                onTap: () {
-                  showLogoutDialog(context, colorScheme);
-                },
-              ),
-              SizedBox(height: 8.h),
             ],
+          ),
+          margin: EdgeInsets.only(top: 35.h, bottom: 16.h),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildPopupHeader(context),
+                const Divider(height: 1, thickness: 1),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+                  child: Obx(() => Text(
+                    homeController.userEmail.value.isNotEmpty
+                        ? homeController.userEmail.value
+                        : '$user',
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      color: colorScheme.onSurface,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  )),
+                ),
+                ...menuItems.map((item) => _buildMenuItem(context, item)),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: const Divider(),
+                ),
+                _buildLogoutTile(context),
+                SizedBox(height: 10.h),
+              ],
+            ),
           ),
         ),
       ),
@@ -224,24 +208,53 @@ class _MenuPopupState extends State<MenuPopup>
   }
 
   Widget _buildPopupHeader(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.w),
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
+      decoration: BoxDecoration(
+        color: colorScheme.surface,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(16.r),
+        ),
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            'MosCheckIn',
-            style: TextStyle(
-              fontSize: 18.sp,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
+          Row(
+            children: [
+              Container(
+                width: 38.w,
+                height: 38.h,
+                decoration: BoxDecoration(
+                  color: colorScheme.primary.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                child: Icon(
+                  Icons.menu,
+                  size: 22.sp,
+                  color: colorScheme.outline,
+                ),
+              ),
+              SizedBox(width: 12.w),
+              Text(
+                'MosCheckIn',
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.bold,
+                  color: colorScheme.onSurface,
+                ),
+              ),
+            ],
           ),
-          GestureDetector(
-            onTap: () {
+          IconButton(
+            onPressed: () {
               _animationController.reverse().then((_) => Get.back());
             },
-            child: Icon(Icons.close, size: 22.sp),
+            icon: Icon(Icons.close, size: 22.sp),
+            style: IconButton.styleFrom(
+              backgroundColor: colorScheme.surfaceVariant,
+              padding: EdgeInsets.all(8.r),
+            ),
           ),
         ],
       ),
@@ -250,36 +263,116 @@ class _MenuPopupState extends State<MenuPopup>
 
   Widget _buildMenuItem(BuildContext context, _MenuItem item) {
     final colorScheme = Theme.of(context).colorScheme;
-    return ListTile(
-      dense: true,
-      visualDensity: const VisualDensity(horizontal: 0, vertical: -1),
-      contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 6.h),
-      leading: Container(
-        width: 36.w,
-        height: 36.h,
-        decoration: BoxDecoration(
-          color: colorScheme.onSurface,
-          shape: BoxShape.circle,
-        ),
-        child: Center(
-          child: Image.asset(
-            item.imagePath,
-            width: 16.w,
-            height: 16.h,
-            fit: BoxFit.contain,
-            color: colorScheme.onPrimary,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: item.onTap,
+        borderRadius: BorderRadius.circular(12.r),
+        splashColor: colorScheme.primary.withOpacity(0.1),
+        highlightColor: colorScheme.primary.withOpacity(0.05),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 12.w, vertical: 2.h),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.r),
+            color: Colors.transparent,
+            border: Border.all(
+              color: colorScheme.surfaceContainerHighest,
+              width: 1.w,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: colorScheme.surface.withOpacity(0.05),
+                spreadRadius: 1.r,
+                blurRadius: 3.r,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 38.w,
+                height: 38.h,
+                decoration: BoxDecoration(
+                  color: colorScheme.primary.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                padding: EdgeInsets.all(8.r),
+                child: Center(
+                  child: Image.asset(
+                    item.imagePath,
+                    width: 22.w,
+                    height: 22.h,
+                    fit: BoxFit.contain,
+                    color: colorScheme.outline,
+                  ),
+                ),
+              ),
+              SizedBox(width: 8.w),
+              Expanded(
+                child: Text(
+                  item.title,
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600,
+                    color: colorScheme.onSurface,
+                    letterSpacing: 0.2,
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.chevron_right_rounded,
+                size: 24.sp,
+                color: colorScheme.onSurfaceVariant.withOpacity(0.7),
+              ),
+            ],
           ),
         ),
       ),
-      title: Text(
-        item.title,
-        style: TextStyle(
-          fontSize: 12.sp,
-          fontWeight: FontWeight.bold,
-          color: colorScheme.onSurface,
+    );
+  }
+
+  Widget _buildLogoutTile(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () => showLogoutDialog(context, colorScheme),
+        borderRadius: BorderRadius.circular(8.r),
+        splashColor: colorScheme.error.withOpacity(0.1),
+        highlightColor: colorScheme.error.withOpacity(0.05),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Row(
+            children: [
+              Container(
+                width: 36.w,
+                height: 36.h,
+                decoration: BoxDecoration(
+                  color: colorScheme.errorContainer,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.logout_rounded,
+                    size: 18.sp,
+                    color: colorScheme.error,
+                  ),
+                ),
+              ),
+              SizedBox(width: 16.w),
+              Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w600,
+                  color: colorScheme.error,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      onTap: item.onTap,
     );
   }
 }
